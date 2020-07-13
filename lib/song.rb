7ae6a51242539e3afb2e01bdee6ca5a06e1fbd12
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-13 13:29:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,42 +1,19 @@
 class Song
-  @@artists=Hash.new
   @@count=0
-  @@genres=Hash.new
-  attr_reader :name, :artist, :genre
+  @@artists=[]
+  @@genres=[]
   def initialize(name,artist,genre)
     @name=name
     @artist=artist
     @genre=genre
-    if @@artists.key?(artist)==false
-      @@artists[artist]=1
-    else
-      @@artists[artist]+=1
-    end
-    if @@genres.key?(genre)==false
-      @@genres[genre]=1
-    else
-      @@genres[genre]+=1
-    end
     @@count+=1
+    @@artists<<artist
+    @genres<<genre
   end
+
+  attr_accessor :name, :genre, :artist
+
   def self.count
     @@count
   end
-  def self.genres
-    @@genres.each do |key,value|
-      key
-    end
-  end
-  def self.artists
-    @@arists.each do |key,value|
-      key
-    end
-  end
-  def self.genre_count
-    @@genres
-  end
-  def self.artist_count
-    @@artists
-  end
 
-end
